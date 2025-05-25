955d09bcc7b6f95701c2eea6416f394d0482884c
MFarisA <muhfarisassami@gmail.com>
2025-05-25 15:21:03 +0700

    Update environment configuration, add new services, and enhance package dependencies
    - Changed APP_NAME in .env.example from 'Laravel' to 'BahasaKita'.
    - Updated APP_URL in .env.example to use 'http://127.0.0.1:8000'.
    - Added new API keys for Gemini, Facebook, and Google in .env.example.
    - Updated composer.json to include new dependencies: dedoc/scramble, filament/filament, guzzlehttp/guzzle, laravel/sanctum, and laravel/socialite.
    - Added a post-autoload script for filament upgrade in composer.json.
    - Updated composer.lock to reflect new dependencies and versions.
    - Configured new services in services.php for Gemini, Facebook, and Google.
    - Added API routing in bootstrap/app.php and registered Filament's AdminPanelProvider in bootstrap/providers.php.

--- config/services.php
+++ config/services.php
@@ -35,4 +35,25 @@ return [
         ],
     ],
 
+    'gemini' => [
+        'api_key' => env('GEMINI_API_KEY'),
+    ],
+
+    // 'google' => [
+    //     'client_id' => env('GOOGLE_CLIENT_ID'),
+    //     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    //     'redirect' => env('GOOGLE_REDIRECT_URI'),
+    // ],
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => '/auth/facebook/callback',
+    ],
+ 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => '/auth/google/callback',
+    ],
+
 ];
