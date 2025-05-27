fc94378e7c79afbbba4769895622876c25ca6627
MFarisA <muhfarisassami@gmail.com>
2025-05-28 00:55:12 +0700

    Add LessonResource and related pages: implement form, table, and relation manager for lesson management

---
+++ app/Filament/Resources/LessonResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LessonResource\Pages;
+use App\Filament\Resources\LessonResource\RelationManagers;
+use App\Models\Lesson;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LessonResource extends Resource
+{
+    protected static ?string $model = Lesson::class;
+
+    protected static ?string $navigationIcon = 'heroicon-s-book-open';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('unit_id')
+                ->label('Level')
+                ->relationship('unit', 'title')
+                ->required(),
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLesson::route('/create'),
+            'edit' => Pages\EditLesson::route('/{record}/edit'),
+        ];
+    }
+}
