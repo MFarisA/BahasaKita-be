96c6039723b9397eea95cbe2b1afa8de1b8ca1f8
MFarisA <muhfarisassami@gmail.com>
2025-05-31 23:56:14 +0700

    Remove Notification system and related routes; update Sanctum expiration setting

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Api\ExerciseSubmissionController;
 use App\Http\Controllers\Api\GetDataController;
 use App\Http\Controllers\Api\LanguageController;
 use App\Http\Controllers\Api\LeaderBoardController;
-use App\Http\Controllers\Api\LeaderBoardGameController;
-use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CulturalContentController;
 use App\Http\Controllers\Auth\GoogleController;
@@ -18,8 +16,8 @@ Route::middleware('auth:sanctum')->withoutMiddleware(['throttle:api'])->get('/us
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
 Route::get('/auth/google/url', [GoogleController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 Route::get('/GetAllLanguage', [LanguageController::class, 'index']);
@@ -42,22 +40,6 @@ Route::middleware('auth:sanctum')->withoutMiddleware(['throttle:api'])->group(fu
     Route::get('/languages/{languageId}/courses/{courseId}/units/{unitId}/sub-units/{subUnitId}/exercises', [ExerciseSubmissionController::class, 'showExercises']);
     Route::get('/languages/{languageId}/courses/{courseId}/units/{unitId}', [ExerciseSubmissionController::class, 'showUnit']);
 
-
-    // Route::get('/notifications', [NotificationController::class, 'index']);
-    // Route::post('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
-
-    // Route::post('/notifications/reminders', [NotificationController::class, 'sendStudyReminders']);
-    // Route::post('/notifications/new-lesson/{lesson}', [NotificationController::class, 'notifyNewLesson']);
-    // Route::post('/notifications/milestones', function () {
-    //     foreach (\App\Models\User::all() as $user) {
-    //         app(NotificationController::class)->checkMilestones($user);
-    //     }
-    // });
-
-    // Route::get('/leaderboard/global', [LeaderBoardGameController::class, 'globalLeaderboard']);
-    // Route::get('/leaderboard/weekly', [LeaderBoardGameController::class, 'weeklyLeaderboard']);
-
-    // Cultural Content Routes
     Route::prefix('cultural-content')->group(function () {
         Route::get('/', [CulturalContentController::class, 'index']);
         Route::get('/{type}', [CulturalContentController::class, 'getByType'])
